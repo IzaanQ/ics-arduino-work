f465a161cf3af9f9c982804e3e9fef44b2f60819
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2022-05-18 09:07:57 -0400

    Create cyclingLights.cpp

---
+++ cyclingLights.cpp
@@ -0,0 +1,23 @@
+int ledPins[]={2,3,4,5,6,7,8,9};
+
+void setup() {
+  for (int i = 0; i < 8; i++){
+    pinMode(ledPins[i], OUTPUT);
+    }
+}
+
+void loop() {
+    oneAfterAnotherNoLoop();
+    
+void oneAfterAnotherLoop()
+{
+  int DelayTime = 100;
+
+  // Turning on the LED
+  for(int i = 0; i <= 8; i++){
+    digitalWrite(ledPins[i], HIGH);
+    digitalWrite(ledPins[i-1], LOW);
+    delay(DelayTime);  
+  }
+  
+}
